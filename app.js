0baea6ecc98e4098b3f2bdb5d7b768208ba5012e
Mwaiwathu Laurent Chatha <151863592+MwaiwathuChatha@users.noreply.github.com>
2024-03-25 19:41:20 +0100

    Major fix added to allow proper preview of uploaded picture.

--- app.js
+++ app.js
@@ -7,35 +7,38 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"))
-/*app.get('/',(req,res)=>{
-  res.render('index');
-})*/
+app.use(express.static(path.join(__dirname,"public")));
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
 
 const multer=require("multer");
 const storage=multer.diskStorage({
   destination: (req,file,cb)=>{
     cb(null,'public')
-  },
-
-filename:(req, file,cb)=>{
+  },filename:(req, file,cb)=>{
   console.log(file);
   cb(null,'image'+path.extname(file.originalname))
+}})
+const upload=multer({storage: storage});
+app.post("/",upload.single("image"),(req,res)=>{
+  res.sendFile(path.join(__dirname+'/secondPage/verification.html'))
+})
+
+const nextpage= function(req,res,next){
+
+  next();
 }
 
-})
+const routeInnit = express.Router(); 
+routeInnit.route('/image').get((req,res)=>{
+  res.sendFile(path.join(__dirname+'/secondPage/verification.html'))
+  })
+app.use('/classify',routeInnit);
+app.use(nextpage);
 
-const upload=multer({storage: storage});
 
 
-
-app.post("/",upload.single("image"),(req,res,next)=>{
-  res.send("image uploaded!");
-  next();
-})
-
-const useRouter=require('./routes/users');
-app.use('/users',useRouter);
 app.listen(port, () => {
  console.log(`Server running on port ${port}`);
 });
